2c8c75b4cd6bc9133f19e77f55d1f4abaab45255
abhishek <abhi.mish.75@gmail.com>
2026-08-09 20:13:52 +0530

    charting libraries tend to be fixed

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // raises the warning threshold to 1MB
+  },
+})
